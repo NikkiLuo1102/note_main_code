9c902efab0831b04ad69d0635726a2aa52f0d215
luoningqi <15277006303@163.com@163.com>
2024-03-06 20:15:50 +0000

    still not add test

--- api/app.py
+++ api/app.py
@@ -4,8 +4,10 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = "http://127.0.0.1:5000/"
+
+API_URL = "http://notesapi.g3cxeje0gvbagsav.uksouth.azurecontainer.io:5000/"
 COLORS = ['Red', 'Green', 'Blue', 'Yellow', 'Orange']
+
 
 def send_request(endpoint, params=None):
     try:
@@ -15,6 +17,7 @@ def send_request(endpoint, params=None):
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
         return None
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,8 +44,9 @@ def index():
             content = request.form['content']
             update_note(user_id, notes[index]['note_id'], color, content)
         return redirect(url_for('index', user_id=user_id))
-    
+
     return render_template('index.html', colors=COLORS, notes=notes)
+
 
 def fetch_notes(user_id):
     response = send_request("retrieve_data", params={"user_id": user_id})
@@ -50,24 +54,24 @@ def fetch_notes(user_id):
         return response
     return []
 
+
 def add_note(user_id, color, content):
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    data = {"user_id": user_id, "color": color, "content": content, "time": current_time}
+    data = {"user_id": user_id, "color": color,
+            "content": content, "time": current_time}
     send_request("create_data", params=data)
+
 
 def delete_note(note_id):
     send_request("delete_data", params={"note_id": note_id})
 
+
 def update_note(user_id, note_id, color, content):
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    data = {"user_id": user_id, "note_id": note_id, "color": color, "content": content, "time": current_time}
+    data = {"user_id": user_id, "note_id": note_id,
+            "color": color, "content": content, "time": current_time}
     send_request("update_data", params=data)
 
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
-
-
-
-
-
-
+    app.run("0.0.0.0", 4000)
